1603e0f698c3de06aa18206c1c437d253817e722
ArcticSaturn <arcticsaturn@gmail.com>
2014-11-21 06:19:32 +0100

    good shape

--- sht21.cpp
+++ sht21.cpp
@@ -8,26 +8,68 @@
 #include "sht21.h"
 
 
-
+SWI2C myI2C (8,9);    // instantiate local softwareI2C class
+  
 
 SHT21::SHT21(uint8_t pinSDA, uint8_t pinSCL)
 {
-  SWI2C myI2C (pinSDA,pinSCL);    // instantiate local softwareI2C class
-  //myI2C.setI2Cpins(pinSDA,pinSCL);
-  myI2C.begin();
-  
+	//SWI2C myI2C (pinSDA,pinSCL);    // instantiate local softwareI2C class
+	myI2C.setI2Cpins(pinSDA,pinSCL);
+	myI2C.begin();
+	
 }
 
-uint8_t SHT21::SoftReset()
+/*
+* Perform soft reset of SHT21
+* registers will be reset to default settings except for heater bit
+* soft reset takes less than 15ms
+*/
+void SHT21::SoftReset()
 {
+	myI2C.i2cStart();
+	myI2C.i2cWrite(I2C_ADDR_W);
+	myI2C.i2cWrite(SOFT_RESET);
+	myI2C.i2cStop();
+	delay(15);	// wait for reset to end
+}
 
 
+/*
+* Read user register of SHT21
+* input: pointer to result variable
+* output: none, result will be written to pointer variable
+*/
+void SHT21::ReadUserRegister(uint8_t *pRegisterValue)
+{
+	myI2C.i2cStart();
+	myI2C.i2cWrite(I2C_ADDR_W);
+	myI2C.i2cWrite(USER_REG_R);
+	myI2C.i2cStart();
+	myI2C.i2cWrite(I2C_ADDR_R);
+	*pRegisterValue = myI2C.i2cRead(1);	// get meas value MSByte
+	myI2C.i2cRead(0);			// get CRC8 value
+	myI2C.i2cStop();
+}
+
+/*
+* Write user register of SHT21
+* input: pointer to variable with new register content
+* output: n/a
+*/
+void SHT21::WriteUserRegister(uint8_t pRegisterValue)
+{
+	myI2C.i2cStart();
+	myI2C.i2cWrite(I2C_ADDR_W);
+	myI2C.i2cWrite(USER_REG_W);
+	myI2C.i2cWrite(pRegisterValue);
+	delayMicroseconds(10);
+	myI2C.i2cStop();
 }
 
 
 // INPUT: 	MeasureType = HUMIDITY, TEMPERATURE
 //			MeasValue = address pointer to variable
-void SHT21::MeasurePolling(enmMeasureType MeasureType, uint16_t* MeasValue, float* MeasValue2)
+void SHT21::MeasurePolling(enmMeasureType MeasureType, uint16_t *pRawMeasValue, float *pMeasValue)
 {
 	uint8_t data1, data2;
 
@@ -49,15 +91,68 @@ void SHT21::MeasurePolling(enmMeasureType MeasureType, uint16_t* MeasValue, floa
 	myI2C.i2cRead(0);           // get CRC8 value
 	myI2C.i2cStop();        // end I2C transmission
 	
-	*MeasValue=(uint16_t)data1*256;
-	*MeasValue+=data2;
-	*MeasValue2=CalcTemperature(*MeasValue);
+	*pRawMeasValue=(uint16_t)data1*256;
+	*pRawMeasValue+=data2;
+	*pMeasValue=CalcTemperature(*pRawMeasValue);
 }
 
+void SHT21::Measure(enmMeasureType MeasureType, uint8_t Resolution, 
+		uint16_t *pRawMeasValue, float *pMeasValue)
+{
+	uint8_t _data1, _data2;
+	uint8_t _UsrRegValue;
+	
+	// read user register and set resolution values
+	ReadUserRegister(&_UsrRegValue);
+	_UsrRegValue = (_UsrRegValue & ~RES_MASK) | Resolution;
+	//WriteUserRegister(_UsrRegValue);
+	WriteUserRegister(Resolution);
+	
+	
+	myI2C.i2cStart();
+	myI2C.i2cWrite(I2C_ADDR_W);
+	// send measurement trigger depending on measurement type
+	if(MeasureType == TEMPERATURE)
+		myI2C.i2cWrite2(START_T_MEAS_POLL);   // trigger Temp measure; no hold master
+	else
+		myI2C.i2cWrite2(START_RH_MEAS_POLL);   // trigger RH measure; no hold master
+	
+	/*delay(150);
+	myI2C.i2cStart();
+	myI2C.i2cWrite(I2C_ADDR_R);
+	*/
+	// polling until slave send acknowledge bit
+	do {
+		delay(10);
+		myI2C.i2cStart();       
+	}while(	myI2C.i2cWrite(I2C_ADDR_R) == NO_I2C_ACK ); 
+		
+	
+	// read measurement value
+	_data1 = myI2C.i2cRead(1);   // get meas value MSByte
+	_data2 = myI2C.i2cRead(1);   // get meas value LSByte
+	myI2C.i2cRead(0);           // get CRC8 value
+	myI2C.i2cStop();        // end I2C transmission
+	
+	// conversion to float values
+	*pRawMeasValue=(uint16_t)_data1*256;	// shift 8 times to left
+	_data2&=0xFC;				// clear bit1/bit0 as these are status bits
+	*pRawMeasValue+=_data2;	
+	if(MeasureType == TEMPERATURE)
+		*pMeasValue=CalcTemperature(*pRawMeasValue);
+	else
+		*pMeasValue=CalcHumidity(*pRawMeasValue);
+}
 
 float SHT21::CalcTemperature(uint16_t rawTemperature)
 {
 	float result;
-	result=-46.85 + 175.72*(rawTemperature)/65536;
+	result=-46.85 + 175.72*(float)rawTemperature/65536;
+	return result;
+}
+float SHT21::CalcHumidity(uint16_t rawHumidity)
+{
+	float result;
+	result=-6+125*(float)rawHumidity/65536;
 	return result;
 }
--- sht21.h
+++ sht21.h
@@ -1,7 +1,10 @@
 /*
-  Morse.h - Library for flashing Morse code.
-  Created by David A. Mellis, November 2, 2007.
-  Released into the public domain.
+* SHT21.h header file for SHT21 library for Energia
+* 
+* Author: 	ArcticSaturn
+* Date:		Nov 2014
+* Rev:		1.0
+*
 */
 #ifndef SHT21_h
 #define SHT21_h
@@ -10,25 +13,64 @@
 #include <SoftwareI2C.h>
 
 
+/*
+* General defines
+*/
+#define START_T_MEAS_HM		0xE3 	// start temperature meas. hold master
+#define START_RH_MEAS_HM	0xE5 	// start humidity meas. hold master
+#define START_T_MEAS_POLL	0xF3 	// start temperature meas. no hold master
+#define START_RH_MEAS_POLL	0xF5 	// start humidity meas. no hold master
+#define USER_REG_W		0xE6	// command writing user register
+#define USER_REG_R		0xE7	// command reading user register
+#define SOFT_RESET		0xFE	// command soft reset
+
+#define RES_12_14BIT		0x00	// RH=12bit, T=14bit
+#define RES_8_12BIT		0x01	// RH= 8bit, T=12bit
+#define RES_10_13BIT		0x80	// RH=10bit, T=13bit
+#define RES_11_11BIT		0x81	// RH=11bit, T=11bit
+
+#define RH_8BIT			0x3B	// RH=8bit,  was 0x01
+#define RH_10BIT		0xBA	// RH=10bit, was 0x80
+#define RH_11BIT		0xBB	// RH=11bit, was 0x81
+#define RH_12BIT		0x3A	// RH=12bit, was 0x00
+
+#define T_11BIT			0x83	// RH=11bit
+#define T_12BIT			0x03	// RH=12bit
+#define T_13BIT			0x82	// RH=13bit
+#define T_14BIT			0x02	// RH=14bit
+
+#define RES_MASK		0x81	// Mask for res. bits (7,0) in user reg.
+
+#define EOB_O			0x40	// end of battery
+#define EOB_MASK		0x40	// Mask for EOB bit(6) in user reg.
+
+#define HEATER_ON		0x04	// heater on
+#define HEATER_OFF		0x00	// heater off
+#define HEATER_MASK		0x04	// Mask for Heater bit(2) in user reg.
+
+#define I2C_ADDR_W		0x80	// I2C address for write access
+#define I2C_ADDR_R		0x81	// I2C address for read access
 
 // measurement signal selection
 typedef enum{
 HUMIDITY,
-TEMP
+TEMPERATURE
 }enmMeasureType;
 
 class SHT21
 {
   public:
 	SHT21(uint8_t pinSDA, uint8_t pinSCL);
-    uint8_t SoftReset();
-	void MeasurePolling(enmMeasureType MeasureType, uint16_t* MeasValue, float* MeasValue2);
+	void SoftReset();
+	void MeasurePolling(enmMeasureType MeasureType, uint16_t *pRawMeasValue, float *pMeasValue);
+	void Measure(enmMeasureType MeasureType, uint8_t Resolution, uint16_t *pRawMeasValue, float *pMeasValue);
 	
-  private:
-    int _pin;
-	float CalcRH(uint16_t rawRH);
+  //private:
+	//int _pin;
 	float CalcTemperature(uint16_t rawTemperature);
-	
+	float CalcHumidity(uint16_t rawHumidity);
+	void ReadUserRegister(uint8_t *pRegisterValue);
+	void WriteUserRegister(uint8_t pRegisterValue);
 };
 
 #endif
